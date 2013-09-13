b577dbb69ddcbb88fd95c3b43c81050fc1791bd0
TheKnightArgent <github@jsiii.com>
2013-09-12 18:58:06 -0700

    Added validation on email uniqueness

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,25 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "without an email address" do
+    it "is not valid" do
+      user = User.new
+      user.should_not be_valid
+    end
+
+    it "fails validation" do
+      expect(User.new).to have(1).error_on(:email)
+    end
+  end
+  context "with an email address" do
+    it "has a unique email address" do
+      u1 = User.new
+      u1.email = "a@b.com"
+      u1.save
+      u2 = User.new
+      u2.email = "a@b.com"
+      u2.should_not be_valid
+    end
+
+  end
 end
